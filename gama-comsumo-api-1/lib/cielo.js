8cd416d9b8fcd0df8379dc2594fc094b65af84bb
Arthur Candido <103594555+oArthurCandido@users.noreply.github.com>
2022-06-09 19:03:08 -0300

    commiting in the end of class 7, everything ok

--- gama-comsumo-api-1/lib/cielo.js
+++ gama-comsumo-api-1/lib/cielo.js
@@ -2,22 +2,32 @@ const fetch = (...args) =>
   import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 class Cielo {
-  static async compra(params) {
-    const response = await fetch(
-      'https://apisandbox.cieloecommerce.cielo.com.br/1/sales/',
+  static compra(params) {
+    return fetch('https://apisandbox.cieloecommerce.cielo.com.br/1/sales/', {
+      method: 'post',
+      body: JSON.stringify(params),
+      headers: {
+        'Content-Type': 'application/json',
+        MerchantId: '82630627-4677-44a3-a96b-371334997e0f',
+        MerchantKey: 'FVIBVSZKJZQIHGPLIJEVEWFMEAPLEDOMAMXNHUJF'
+      }
+    }).then(res => res.json());
+  }
+
+  static captura(paymentId) {
+    return fetch(
+      'https://apisandbox.cieloecommerce.cielo.com.br/1/sales/' +
+        paymentId +
+        '/capture',
       {
-        method: 'post',
-        body: JSON.stringify(params),
+        method: 'put',
         headers: {
           'Content-Type': 'application/json',
           MerchantId: '82630627-4677-44a3-a96b-371334997e0f',
           MerchantKey: 'FVIBVSZKJZQIHGPLIJEVEWFMEAPLEDOMAMXNHUJF'
         }
       }
-    );
-    const data = await response.json();
-
-    console.log(data);
+    ).then(res => res.json());
   }
 }
 
