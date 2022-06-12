927d7e4ad6f0690757ae8695e5b51cf2c104c71c
Arthur Candido <103594555+oArthurCandido@users.noreply.github.com>
2022-06-11 22:10:19 -0300

    alterando para versão 5 da api pagarme

--- gama-consumo-api-3/lib/pagarme.js
+++ gama-consumo-api-3/lib/pagarme.js
@@ -1,16 +1,42 @@
-const axios = require('axios');
+const axios = require('axios').default;
 
 class Pagarme {
   static compra(params) {
-    return axios.post('https://api.pagar.me/1/transactions', params, {
+    return axios.post('https://api.pagar.me/core/v5/orders/', params, {
       headers: {
-        'content-type': 'application.json'
+        Accept: 'application/json',
+        Authorization: 'Basic c2tfdGVzdF9OV3k4a0pWWGhqQzZrOXpQOg==',
+        'Content-Type': 'application/json'
       }
     });
   }
 
-  static captura(paymentId) {}
-  static consulta(paymentId) {}
+  // Deixei comentado o trecho abaixo, pq ao invés de fazer o pedido e depois a captura, unifiquei o pedido com a captura alterando o body da requisição de compra.
+  // static captura(paymentId) {
+  //   return axios.post(
+  //     `https://api.pagar.me/core/v5/charges/${paymentId}/capture`,
+  //     {
+  //       headers: {
+  //         Accept: 'application/json',
+  //         Authorization: 'Basic c2tfdGVzdF9OV3k4a0pWWGhqQzZrOXpQOg==',
+  //         ContentType: 'application/json'
+  //       }
+  //     }
+  //   );
+  // }
+
+  static consulta(order_id) {
+    return axios.get(
+      `https://api.pagar.me/core/v5/orders/${order_id}`,
+      {},
+      {
+        headers: {
+          Accept: 'application/json',
+          Authorization: 'Basic c2tfdGVzdF9OV3k4a0pWWGhqQzZrOXpQOg=='
+        }
+      }
+    );
+  }
 }
 
 module.exports = Pagarme;
--- gama-consumo-api-3/routes/compras.js
+++ gama-consumo-api-3/routes/compras.js
@@ -1,69 +1,68 @@
 var express = require('express');
 var router = express.Router();
-var pagarme = require('../lib/pagarme.js');
+var pagarme = require('../lib/pagarme');
 /* POST criação de compras. */
 router.post('/', function (req, res, next) {
   pagarme
     .compra(req.body)
     .then(result => {
-      const statusPagarme = result.Payment.Status;
-      const paymentId = result.Payment.PaymentId;
-      pagarme
-        .captura(paymentId)
-        .then(result => {
-          if (statusPagarme == 2) {
-            res.status(201).send({
-              Status: 'Ok',
-              Message: 'Compra realizada com sucesso!',
-              CompraId: paymentId
-            });
-          } else {
-            res.status(402).send({
-              Status: 'Falhou',
-              Message:
-                'Compra não realizada, problema na cobrança com cartão de crédito =('
-            });
-          }
-        })
-        .catch(err => {
-          console.error(err);
+      const order_id = result.data.id;
+      if (result.data.status == 'paid') {
+        res.status(201).send({
+          Status: 'Ok ',
+          Message: 'Compra realizada com sucesso!',
+          Id: order_id
         });
+      } else {
+        res.status(402).send({
+          Status: 'Falhou',
+          Message:
+            'Compra não realizada, problema na cobrança com cartão de crédito =('
+        });
+      }
     })
-    .catch(function (error) {
-      console.error(error);
+    .catch(err => {
+      console.error(err);
     });
 });
 
 /* GET status de compras. */
 router.get('/:compra_id/status/', function (req, res, next) {
-  pagarme.consulta(req.params.compra_id).then(result => {
-    const statusPagarme = result.Payment.Status;
-    let message = {};
+  pagarme
+    .consulta(req.params.compra_id)
+    .then(result => {
+      const order_id = result.data.id;
+      console.log(order_id);
+      console.log(result);
+      let message = {};
 
-    switch (result.Payment.Status) {
-      case 1:
-        message = {
-          Status: 'Código ' + statusPagarme + ' - Pagamento autorizado!'
-        };
-        break;
-      case 2:
-        message = {
-          Status:
-            'Código ' + statusPagarme + ' - Pagamento concluído com sucesso!'
-        };
-        break;
-      case 12:
-        message = {
-          Status: 'Código ' + statusPagarme + ' - Pagamento pendente.'
-        };
-        break;
-      default:
-        message = {
-          Status: 'Código ' + statusPagarme + ' - Pagamento falhou.'
-        };
-    }
-    res.send(message);
-  });
+      switch (result.data.id) {
+        case 'authorized':
+          message = {
+            Status: 'Código ' + result.status + ' - Pagamento autorizado!'
+          };
+          break;
+        case 'paid':
+          message = {
+            Status:
+              'Código ' + result.status + ' - Pagamento concluído com sucesso!'
+          };
+          break;
+        case 'processing':
+          message = {
+            Status: 'Código ' + result.status + ' - Pagamento pendente.'
+          };
+          break;
+        default:
+          message = {
+            Status: 'Código ' + result.status + ' - Pagamento falhou.'
+          };
+      }
+      res.send(message);
+    })
+    .catch(err => {
+      console.error(err);
+    });
 });
 
 module.exports = router;
